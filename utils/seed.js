85599d2e4c1ab2f53350fcf8c6307f7ea5ed5b16
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-06-29 11:01:27 -0600

    Update data seeding code

--- utils/seed.js
+++ utils/seed.js
@@ -1,74 +1,74 @@
 // Data to be seeded
 const data = [
-    {
-      "username": "michael.scott",
-      "email": "michael.scott@dm.com"
-    },
-    {
-      "username": "toby.flenderson",
-      "email": "toby.flenderson@dm.com"
-    },
-    {
-      "username": "creed.bratton",
-      "email": "creed.bratton@dm.com"
-    },
-    {
-      "username": "stanley.hudson",
-      "email": "stanley.hudson@dm.com"
-    },
-    {
-      "username": "phyllis.vance",
-      "email": "phyllis.vance@dm.com"
-    },
-    {
-      "username": "dwight.schrute",
-      "email": "dwight.schrute@dm.com"
-    },
-    {
-      "username": "jim.halpert",
-      "email": "jim.halpert@dm.com"
-    },
-    {
-      "username": "angela.schrute",
-      "email": "angela.schrute@dm.com"
-    },
-    {
-      "username": "oscar.martinez",
-      "email": "oscar.martinez@dm.com"
-    },
-    {
-      "username": "pam.halpert",
-      "email": "pam.halpert@dm.com"
-    },
-    {
-      "username": "meredith.palmer",
-      "email": "meredith.palmer@dm.com"
-    },
-    {
-      "username": "kelly.kapoor",
-      "email": "kelly.kapoor@dm.com"
-    },
-    {
-      "username": "andrew.bernard",
-      "email": "andrew.bernard@dm.com"
-    },
-    {
-      "username": "darryl.philbin",
-      "email": "darryl.philbin@dm.com"
-    },
-    {
-      "username": "kevin.malone",
-      "email": "kevin.malone@dm.com"
-    },
-    {
-      "username": "ryan.howard",
-      "email": "ryan.howard@dm.com"
-    },
-    {
-      "username": "erin.hannon",
-      "email": "erin.hannon@dm.com"
-    }
-   ]
+  {
+    username: "michael.scott",
+    email: "michael.scott@dm.com",
+  },
+  {
+    username: "toby.flenderson",
+    email: "toby.flenderson@dm.com",
+  },
+  {
+    username: "creed.bratton",
+    email: "creed.bratton@dm.com",
+  },
+  {
+    username: "stanley.hudson",
+    email: "stanley.hudson@dm.com",
+  },
+  {
+    username: "phyllis.vance",
+    email: "phyllis.vance@dm.com",
+  },
+  {
+    username: "dwight.schrute",
+    email: "dwight.schrute@dm.com",
+  },
+  {
+    username: "jim.halpert",
+    email: "jim.halpert@dm.com",
+  },
+  {
+    username: "angela.schrute",
+    email: "angela.schrute@dm.com",
+  },
+  {
+    username: "oscar.martinez",
+    email: "oscar.martinez@dm.com",
+  },
+  {
+    username: "pam.halpert",
+    email: "pam.halpert@dm.com",
+  },
+  {
+    username: "meredith.palmer",
+    email: "meredith.palmer@dm.com",
+  },
+  {
+    username: "kelly.kapoor",
+    email: "kelly.kapoor@dm.com",
+  },
+  {
+    username: "andrew.bernard",
+    email: "andrew.bernard@dm.com",
+  },
+  {
+    username: "darryl.philbin",
+    email: "darryl.philbin@dm.com",
+  },
+  {
+    username: "kevin.malone",
+    email: "kevin.malone@dm.com",
+  },
+  {
+    username: "ryan.howard",
+    email: "ryan.howard@dm.com",
+  },
+  {
+    username: "erin.hannon",
+    email: "erin.hannon@dm.com",
+  },
+];
 
 const connection = require("../config/connection");
 const { User, Thought } = require("../models");
@@ -98,35 +98,30 @@ connection.once("open", async () => {
   // Create a single thought document
   const michaelsThought = await Thought.create({
     thoughtText: `That's what she said!`,
-    username: 'michael.scott',
+    username: "michael.scott",
   });
 
-  const friends = [];
-
-  for(let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     const username = data[i].username;
     const email = data[i].email;
+    const friends = [];
     const thoughts = [];
 
     // If username is michael.scott then add the ID of the thought previously created
-    if (username === 'michael.scott') {
-        thoughts.push(michaelsThought._id);
+    if (username === "michael.scott") {
+      thoughts.push(michaelsThought._id);
     }
 
     // Create a single user document
-    const userData = await User.create({
-        username,
-        email,
-        friends,
-        thoughts
+    await User.create({
+      username,
+      email,
+      friends,
+      thoughts,
     });
 
-    // Start populating friends array to be used
-    friends.push(userData._id);
   }
 
-
-  console.log('Data seeded!');
+  console.log("Data seeded!");
   process.exit(0);
-
 });
